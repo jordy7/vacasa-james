230f6b2c15dc862495b02cf001c333b74ea3e2c3
jordy7 <armoon@gmail.com>
2022-04-17 23:00:56 +1200

    New component

---
+++ src/pages/RepositoriesPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RepositoriesPage = () => <div>repos page</div>;
+
+export default RepositoriesPage;
---
+++ src/pages/SettingPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SettingPage = () => <div>setting page</div>;
+
+export default SettingPage;
